ffe1292258a66b853a549b467250ebfa3b0680f4
miad253062 <miadac681@gmail.com>
2026-08-16 21:18:48 +0600

    Learning TypeScript

---
+++ first.js
@@ -0,0 +1,12 @@
+"use strict";
+let age = 12;
+if (age >= 18) {
+    console.log("You are an adult");
+}
+else {
+    console.log("Baby!");
+}
+console.log(age);
+function greeting(name) {
+    return `Hi ${name}`;
+}
---
+++ first.ts
@@ -0,0 +1,14 @@
+let age: number = 12;
+
+if(age >= 18){
+    console.log("You are an adult")
+}else{
+    console.log("Baby!")
+}
+console.log(age)
+
+
+
+function greeting(name:string): string {
+    return `Hi ${name}`
+}
