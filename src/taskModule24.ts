6843d2f0b535d04ffea2fa629da60d142d1a6c59
miad253062 <miadac681@gmail.com>
2026-08-20 16:50:35 +0600

    Files moved to src folder

---
+++ src/taskModule24.ts
@@ -0,0 +1,16 @@
+const getAgeRatingAccess = (age: number): string => {
+    if(age < 7){
+        return "E (Everyone)"
+    }else if(age >= 7 && age <=12){
+        return "E10+ (Everyone 10+)"
+    }else if(age >=13 && age <= 16){
+        return "T (Teen)"
+    }else{
+        return "M (Mature)"
+    }
+}
+
+
+const getSignalStatus =(): string=>{
+    return ''
+}
